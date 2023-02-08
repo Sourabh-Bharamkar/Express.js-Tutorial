cfd0183104d83a8b24eb5659e34f3be6d759d6cd
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-02-09 02:30:09 +0530

    MVC-added controllers

---
+++ Express Tutorial- Udemy Trainer/routes/admin.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const productsControllers=require('../controllers/products')
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product',productsControllers.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', productsControllers.postAddProduct);
+
+exports.routes = router;
+exports.products = products;
---
+++ Express Tutorial- Udemy Trainer/routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+const productsControllers=require('../controllers/products')
+
+const router = express.Router();
+
+router.get('/', productsControllers.getProducts);
+
+module.exports = router;
